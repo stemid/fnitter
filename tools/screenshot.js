203065ec52101244d03948247dbbe7739931f2f5
Stefan Midjich <swehack@gmail.com>
2014-03-23 21:51:12 +0100

    WHY did phantomjs exclude process from Node.js? well I am just outputting the filename at the end so celery task knows about it

--- tools/screenshot.js
+++ tools/screenshot.js
@@ -1,7 +1,10 @@
+// Take screenshots of webpages with PhantomJS
+// By Stefan Midjich
+//
 var page = require('webpage').create();
 var sys = require('system');
 
-var url = '', img_b64 = '', md5sum = '', output_dir = '';
+var url = '', output_dir = '';
 
 function print_usage () {
   console.log('Usage: phantomjs screenshot.js <url> [output dir]');
@@ -16,18 +19,23 @@ if (sys.args.length === 1) {
 }
 
 page.open(url, function () {
+  var img_b64 = '', md5sum = '', filename = '';
+
   if (!phantom.injectJs('md5.js')) {
     phantom.exit();
   }
   img_b64 = page.renderBase64('png');
   md5sum = CryptoJS.MD5(img_b64);
 
+  filename = md5sum + '_img.png';
+
   if (output_dir === undefined) {
-    output_file = md5sum + '_img.png';
+    output_file = filename;
   } else {
-    output_file = output_dir + '/' + md5sum + '_img.png';
+    output_file = output_dir + '/' + filename;
   }
 
   page.render(output_file);
+  console.log(filename);
   phantom.exit();
 });
